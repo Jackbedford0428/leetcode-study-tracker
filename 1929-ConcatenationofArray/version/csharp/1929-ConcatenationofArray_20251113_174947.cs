6973aed934865feee367f275e78c13e6010d4aa0
Jackbedford0428 <81814632+Jackbedford0428@users.noreply.github.com>
2025-11-13 17:49:47 +0800

    Create 1929-ConcatenationofArray

---
+++ 1929-ConcatenationofArray/version/csharp/1929-ConcatenationofArray_20251113_174947.cs
@@ -0,0 +1,12 @@
+// Last updated: 2025/11/13 下午5:49:47
+public class Solution {
+    public int[] GetConcatenation(int[] nums) {
+        int n = nums.Length;
+        int[] ans = new int[n * 2];
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+        return ans;
+    }
+}
