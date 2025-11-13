4ec13aef191e63c7ca54b1610b2153369bde8572
Jackbedford0428 <81814632+Jackbedford0428@users.noreply.github.com>
2025-11-13 17:52:46 +0800

    Create 1929-ConcatenationofArray

---
+++ 1929-ConcatenationofArray/version/java/1929-ConcatenationofArray_20251113_175245.java
@@ -0,0 +1,12 @@
+// Last updated: 2025/11/13 下午5:52:45
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n * 2];
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+        return ans;
+    }
+}
