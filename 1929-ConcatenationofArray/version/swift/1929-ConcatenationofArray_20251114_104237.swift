65a74ca875d3632b752b338bdd53f22600b3540f
Jackbedford0428 <81814632+Jackbedford0428@users.noreply.github.com>
2025-11-14 10:42:38 +0800

    Create 1929-ConcatenationofArray

---
+++ 1929-ConcatenationofArray/version/swift/1929-ConcatenationofArray_20251114_104237.swift
@@ -0,0 +1,13 @@
+// Last updated: 2025/11/14 上午10:42:37
+class Solution {
+    func getConcatenation(_ nums: [Int]) -> [Int] {
+        // let n = nums.count
+        // var ans = [Int](repeating: 0, count: n * 2)
+        // for i in 0..<n {
+        //     ans[i] = nums[i]
+        //     ans[i + n] = nums[i]
+        // }
+        // return ans
+        return nums + nums
+    }
+}
