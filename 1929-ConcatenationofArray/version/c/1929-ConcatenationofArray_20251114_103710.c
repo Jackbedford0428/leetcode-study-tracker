0d5106771b283353f58d8faa5e0a6b5e8f9cb812
Jackbedford0428 <81814632+Jackbedford0428@users.noreply.github.com>
2025-11-14 10:37:10 +0800

    Create 1929-ConcatenationofArray

---
+++ 1929-ConcatenationofArray/version/c/1929-ConcatenationofArray_20251114_103710.c
@@ -0,0 +1,15 @@
+// Last updated: 2025/11/14 上午10:37:10
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* getConcatenation(int* nums, int numsSize, int* returnSize) {
+    *returnSize = numsSize * 2;
+    int* ans = (int*)malloc((*returnSize) * sizeof(int));
+    // for (int i = 0; i < numsSize; i++) {
+    //     ans[i] = nums[i];
+    //     ans[i + numsSize] = nums[i];
+    // }
+    memcpy(ans, nums, numsSize * sizeof(int));
+    memcpy(ans + numsSize, nums, numsSize * sizeof(int));
+    return ans;
+}
