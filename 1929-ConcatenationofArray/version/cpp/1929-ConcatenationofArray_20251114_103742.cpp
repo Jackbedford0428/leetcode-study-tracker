c8c16544c35d571edcc2f9e52a1c20c65e912e8d
Jackbedford0428 <81814632+Jackbedford0428@users.noreply.github.com>
2025-11-14 10:37:43 +0800

    Create 1929-ConcatenationofArray

---
+++ 1929-ConcatenationofArray/version/cpp/1929-ConcatenationofArray_20251114_103742.cpp
@@ -0,0 +1,13 @@
+// Last updated: 2025/11/14 上午10:37:42
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        // vector<int> ans;
+        // ans.reserve(nums.size() * 2);
+        // ans.insert(ans.end(), nums.begin(), nums.end());
+        // ans.insert(ans.end(), nums.begin(), nums.end());
+        vector<int> ans(nums);
+        ans.insert(ans.end(), nums.begin(), nums.end());
+        return ans;
+    }
+};
