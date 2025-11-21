982c4563fc17ed38b029fb4e62fc3ff9cee4aa0e
Jackbedford0428 <81814632+Jackbedford0428@users.noreply.github.com>
2025-11-21 10:16:04 +0800

    Create 1929-ConcatenationofArray

---
+++ 1929-ConcatenationofArray/version/typescript/1929-ConcatenationofArray_20251121_101604.ts
@@ -0,0 +1,14 @@
+// Last updated: 2025/11/21 上午10:16:04
+function findMaxConsecutiveOnes(nums: number[]): number {
+    let maxCount: number = 0;
+	let currentCount: number = 0;
+	for (const num of nums) {
+	    if (num === 1) {
+	        currentCount++;
+	        maxCount = Math.max(maxCount, currentCount);
+	    } else {
+	        currentCount = 0;
+	    }
+	}
+	return maxCount;
+};
