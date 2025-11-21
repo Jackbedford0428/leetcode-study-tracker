fd1d72cd4d347b9360ee92076639d3809a1ee2da
Jackbedford0428 <81814632+Jackbedford0428@users.noreply.github.com>
2025-11-21 10:24:59 +0800

    Create 1929-ConcatenationofArray

---
+++ 1929-ConcatenationofArray/version/kotlin/1929-ConcatenationofArray_20251121_102458.kt
@@ -0,0 +1,16 @@
+// Last updated: 2025/11/21 上午10:24:58
+class Solution {
+    fun findMaxConsecutiveOnes(nums: IntArray): Int {
+        var maxCount = 0
+        var currentCount = 0
+        nums.forEach { num ->
+            if (num == 1) {
+                currentCount++
+                maxCount = maxOf(maxCount, currentCount)
+            } else {
+                currentCount = 0
+            }
+        }
+        return maxCount
+    }
+}
