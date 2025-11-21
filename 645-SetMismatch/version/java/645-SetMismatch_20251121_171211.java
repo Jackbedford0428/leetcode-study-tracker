41cf96dd39e8660f274feeb1fbb68df861d0ce25
Jackbedford0428 <81814632+Jackbedford0428@users.noreply.github.com>
2025-11-21 17:12:12 +0800

    Create 645-SetMismatch

---
+++ 645-SetMismatch/version/java/645-SetMismatch_20251121_171211.java
@@ -0,0 +1,23 @@
+// Last updated: 2025/11/21 下午5:12:11
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        int expected_sum = n * (n + 1) / 2;
+        int actual_sum = 0;
+        long expected_square_sum = (long)n * (n + 1) * (2 * n + 1) / 6;
+        long actual_square_sum = 0;
+        
+        for (int num : nums) {
+            actual_sum += num;
+            actual_square_sum += (long)num * num;
+        }
+        
+        int diff_xy = actual_sum - expected_sum;
+        long square_diff_xy = actual_square_sum - expected_square_sum;
+        int sum_xy = (int)(square_diff_xy / diff_xy);
+        
+        int duplicate = (sum_xy + diff_xy) / 2;
+        int missing = sum_xy - duplicate;
+        return new int[] { duplicate, missing };
+    }
+}
