68b18d6cdcf0dc50f00015b21f60698754a9ca36
Jackbedford0428 <81814632+Jackbedford0428@users.noreply.github.com>
2025-11-21 10:30:51 +0800

    Create 1929-ConcatenationofArray

---
+++ 1929-ConcatenationofArray/version/php/1929-ConcatenationofArray_20251121_103050.php
@@ -0,0 +1,21 @@
+// Last updated: 2025/11/21 上午10:30:50
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function findMaxConsecutiveOnes($nums) {
+        $maxCount = 0;
+        $currentCount = 0;
+        foreach ($nums as $num) {
+            if ($num === 1) {
+                $currentCount++;
+                $maxCount = max($maxCount, $currentCount);
+            } else {
+                $currentCount = 0;
+            }
+        }
+        return $maxCount;
+    }
+}
