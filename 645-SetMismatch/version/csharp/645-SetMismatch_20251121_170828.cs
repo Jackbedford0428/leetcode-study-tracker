f63d8f809ee204a88b63497016cd7d7854d9cd91
Jackbedford0428 <81814632+Jackbedford0428@users.noreply.github.com>
2025-11-21 17:08:31 +0800

    Create 645-SetMismatch

---
+++ 645-SetMismatch/version/csharp/645-SetMismatch_20251121_170828.cs
@@ -0,0 +1,19 @@
+// Last updated: 2025/11/21 下午5:08:28
+public class Solution {
+    public int[] FindErrorNums(int[] nums) {
+        int n = nums.Length;
+        int expected_sum = n * (n + 1) / 2;
+        int actual_sum = nums.Sum();
+        
+        long expected_square_sum = (long)n * (n + 1) * (2 * n + 1) / 6;
+        long actual_square_sum = nums.Sum(x => (long)x * x);
+        
+        int diff_xy = actual_sum - expected_sum;
+        long square_diff_xy = actual_square_sum - expected_square_sum;
+        int sum_xy = (int)(square_diff_xy / diff_xy);
+        
+        int duplicate = (sum_xy + diff_xy) / 2;
+        int missing = sum_xy - duplicate;
+        return new int[] { duplicate, missing };
+    }
+}
