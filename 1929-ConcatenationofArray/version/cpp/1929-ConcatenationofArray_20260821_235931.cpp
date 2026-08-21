90ed170af43a3fd57729336e2aebcca9eff0811b
Jackbedford0428 <81814632+Jackbedford0428@users.noreply.github.com>
2026-08-21 23:59:31 +0800

    Create 1929-ConcatenationofArray

---
+++ 1929-ConcatenationofArray/version/cpp/1929-ConcatenationofArray_20260821_235931.cpp
@@ -0,0 +1,9 @@
+// Last updated: 2026/8/21 下午11:59:31
+1class Solution {
+2public:
+3    vector<int> getConcatenation(vector<int>& nums) {
+4        vector<int> ans(nums);
+5        ans.insert(ans.end(), nums.begin(), nums.end());
+6        return ans;
+7    }
+8};
