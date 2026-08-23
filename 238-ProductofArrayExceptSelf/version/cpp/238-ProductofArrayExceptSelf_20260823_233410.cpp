7c8951e553e6a05b257bacc7c9d2eba7c8a11939
Jackbedford0428 <81814632+Jackbedford0428@users.noreply.github.com>
2026-08-23 23:34:10 +0800

    Create 238-ProductofArrayExceptSelf

---
+++ 238-ProductofArrayExceptSelf/version/cpp/238-ProductofArrayExceptSelf_20260823_233410.cpp
@@ -0,0 +1,23 @@
+// Last updated: 2026/8/23 下午11:34:10
+1class Solution {
+2public:
+3    vector<int> productExceptSelf(vector<int>& nums) {
+4        int n = nums.size();
+5        vector<int> ans(n);
+6        // set `ans` as suffix product array
+7        ans[n-1] = nums[n-1];
+8        for (int i = n - 2; i >= 0; i--)
+9            ans[i] = ans[i+1] * nums[i];
+10        // set `nums` as prefix product array
+11        for (int i = 1; i < n; i++)
+12            nums[i] = nums[i-1] * nums[i];
+13        // finalize
+14        ans[0] = ans[1];
+15        for (int i = 1; i < n - 1; i++) {
+16            ans[i] = nums[i-1] * ans[i+1];
+17        }
+18        ans[n-1] = nums[n-2];
+19        return ans;
+20    }
+21};
+22
