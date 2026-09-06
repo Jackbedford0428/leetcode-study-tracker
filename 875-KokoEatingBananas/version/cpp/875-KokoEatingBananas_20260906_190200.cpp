a27939d411d6b6ff878ab6755b63d49044ffc5a9
Jackbedford0428 <81814632+Jackbedford0428@users.noreply.github.com>
2026-09-06 19:02:00 +0800

    Create 875-KokoEatingBananas

---
+++ 875-KokoEatingBananas/version/cpp/875-KokoEatingBananas_20260906_190200.cpp
@@ -0,0 +1,24 @@
+// Last updated: 2026/9/6 下午7:02:00
+1class Solution {
+2public:
+3    int minEatingSpeed(vector<int>& piles, int h) {
+4        int max_pile = *max_element(piles.begin(), piles.end());
+5        int n = static_cast<int>(piles.size());
+6        // vector<int> speeds(max_pile + 1);
+7        // iota(speeds.begin(), speeds.end(), 0);
+8        int low = 1, high = max_pile + 1;
+9        while (low < high) {
+10            int mid = low + (high - low) / 2;
+11            // int speed = speeds[mid], need = 0;
+12            long long need = 0;
+13            for (int pile : piles) {
+14                need += (static_cast<long long>(pile) + mid - 1) / mid;
+15            }
+16            if (need > h)
+17                low = mid + 1;
+18            else
+19                high = mid;
+20        }
+21        return low;
+22    }
+23};
