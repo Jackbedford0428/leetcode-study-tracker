9e09ea4d0d8a3bbc18d40d8c86cb7751234c1430
Jackbedford0428 <81814632+Jackbedford0428@users.noreply.github.com>
2026-09-17 13:49:26 +0800

    Create 133-CloneGraph

---
+++ 133-CloneGraph/version/cpp/133-CloneGraph_20260917_134926.cpp
@@ -0,0 +1,46 @@
+// Last updated: 2026/9/17 下午1:49:26
+1/*
+2// Definition for a Node.
+3class Node {
+4public:
+5    int val;
+6    vector<Node*> neighbors;
+7    Node() {
+8        val = 0;
+9        neighbors = vector<Node*>();
+10    }
+11    Node(int _val) {
+12        val = _val;
+13        neighbors = vector<Node*>();
+14    }
+15    Node(int _val, vector<Node*> _neighbors) {
+16        val = _val;
+17        neighbors = _neighbors;
+18    }
+19};
+20*/
+21
+22class Solution {
+23public:
+24    Node* cloneGraph(Node* node) {
+25        if (node == nullptr)
+26            return node;
+27
+28        unordered_map<Node *, Node *> clones;
+29        stack<Node *> stk;
+30        clones[node] = new Node(node->val);
+31        stk.push(node);
+32        while (!stk.empty()) {
+33            Node *cur = stk.top();
+34            stk.pop();
+35            for (Node *nei : cur->neighbors) {
+36                if (!clones.contains(nei)) {
+37                    clones[nei] = new Node(nei->val);
+38                    stk.push(nei);
+39                }
+40                clones[cur]->neighbors.push_back(clones[nei]);
+41            }
+42        }
+43        return clones[node];
+44    }
+45};
