0748afef16b6ee7c0f000e47de89d3243ac8c6f8
Jackbedford0428 <81814632+Jackbedford0428@users.noreply.github.com>
2026-09-24 09:45:01 +0800

    Create 78-Subsets

---
+++ 78-Subsets/version/cpp/78-Subsets_20260924_094500.cpp
@@ -0,0 +1,22 @@
+// Last updated: 2026/9/24 上午9:45:00
+1class Solution {
+2public:
+3    vector<vector<int>> subsets(vector<int>& nums) {
+4        vector<vector<int>> res;
+5        vector<int> path;
+6        backtrack(nums, 0, path, res);
+7        return res;
+8    }
+9
+10private:
+11    void backtrack(vector<int> &nums, int level, vector<int> &path, vector<vector<int>> &res) {
+12        if (level == static_cast<int>(nums.size())) {
+13            res.push_back(path);
+14            return;
+15        }
+16        path.push_back(nums[level]);
+17        backtrack(nums, level + 1, path, res);
+18        path.pop_back();
+19        backtrack(nums, level + 1, path, res);
+20    }
+21};
